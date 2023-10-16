fd743a5fc4f4aa56fcdd316f1f5576356f19dd59
fadimanakilci <fadimekilci07@gmail.com>
2023-10-16 13:20:08 +0300

    Created Project

---
+++ src/main/java/com/reinforcement/project/ProjectApplication.java
@@ -0,0 +1,14 @@
+package com.reinforcement.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		System.out.println("sdfghjklş");
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+
+}
