1c66004703f9f296fc1666b04353d6e1cfa59788
fadimanakilci <fadimekilci07@gmail.com>
2023-10-23 14:44:22 +0300

    Added Instancio library

--- src/main/java/com/reinforcement/project/ProjectApplication.java
+++ src/main/java/com/reinforcement/project/ProjectApplication.java
@@ -1,18 +1,25 @@
 package com.reinforcement.project;
 
+import com.reinforcement.project.entities.concretes.Brand;
+import org.instancio.Instancio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.Nonnull;
+import java.util.List;
 
 @SpringBootApplication
 public class ProjectApplication {
 
 	public static void main(String[] args) {
-		System.out.println("sdfghjklş");
 		foo();
+		List<Brand> brands = Instancio.ofList(Brand.class).size(10).create();
+		for (Brand brand : brands) {
+			System.out.println("args = " + brand.getName() + " - " + brand.getId());
+		}
 		SpringApplication.run(ProjectApplication.class, args);
 	}
+
 
 	static void log(@Nonnull Object x) {
 		System.out.println("x = " + x);
